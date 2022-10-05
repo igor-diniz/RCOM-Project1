65ae453a209531daea3fc283f1638f9fb9e8bc1c
luis <joseluiscunharodrigues@gmail.com>
2022-10-05 19:14:45 +0100

    Implemented link layer functions

--- src/src/link_layer.c
+++ src/src/link_layer.c
@@ -2,15 +2,65 @@
 
 #include "link_layer.h"
 
+static int fd;
+static struct termios oldtio;
+static struct termios newtio;
+
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
+
+#define BUF_SIZE 256
 
 ////////////////////////////////////////////////
 // LLOPEN
 ////////////////////////////////////////////////
 int llopen(LinkLayer connectionParameters)
 {
-    // TODO
+    fd = open(connectionParameters.serialPort, O_RDWR | O_NOCTTY);
+    if (fd < 0)
+    {
+        perror(connectionParameters.serialPort);
+        exit(-1);
+    }
+
+
+    // Save current port settings
+    if (tcgetattr(fd, &oldtio) == -1)
+    {
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    // Clear struct for new port settings
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = connectionParameters.baudRate | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode (non-canonical, no echo,...)
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0; // Inter-character timer unused
+    newtio.c_cc[VMIN] = 1;  // Blocking read until 1 char received
+
+    // VTIME e VMIN should be changed in order to protect with a
+    // timeout the reception of the following character(s)
+
+    // Now clean the line and activate the settings for the port
+    // tcflush() discards data written to the object referred to
+    // by fd but not transmitted, or data received but not read,
+    // depending on the value of queue_selector:
+    //   TCIFLUSH - flushes data received but not read.
+    tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
 
     return 1;
 }
@@ -20,9 +70,7 @@ int llopen(LinkLayer connectionParameters)
 ////////////////////////////////////////////////
 int llwrite(const unsigned char *buf, int bufSize)
 {
-    // TODO
-
-    return 0;
+    return write(fd, buf, bufSize);
 }
 
 ////////////////////////////////////////////////
@@ -30,9 +78,23 @@ int llwrite(const unsigned char *buf, int bufSize)
 ////////////////////////////////////////////////
 int llread(unsigned char *packet)
 {
-    // TODO
+    // Loop for input
+    int loop = 1;
+    int nChars = 0;
 
-    return 0;
+    while (loop)
+    {
+        // Returns after 5 chars have been input
+        int bytes = read(fd, packet + nChars, 1);
+        if (bytes == -1) return -1;
+        nChars += bytes;
+
+        if (packet[nChars] == '\0') {
+            loop = 0;
+        }
+    }
+
+    return nChars;
 }
 
 ////////////////////////////////////////////////
@@ -40,7 +102,14 @@ int llread(unsigned char *packet)
 ////////////////////////////////////////////////
 int llclose(int showStatistics)
 {
-    // TODO
+    // Restore the old port settings
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    close(fd);
 
     return 1;
 }
